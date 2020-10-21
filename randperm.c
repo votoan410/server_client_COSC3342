f2f42030b4f08195bd527e1e52af93d94dfb8885
TMV <gaius@alphabetindex0addMY>
2020-10-21 12:37:48 -0500

    .

---
+++ randperm.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+//The function randperm randomly permutes the first N elements
+//of an array r
+int randperm(int *r,int N)
+{
+        int i,j;
+        int temp;
+        srand(time(0));
+
+        for (i = N-1; i >= 0; --i)
+        {
+    //generate a random number [0, n-1]
+                j = rand() % (i+1);
+    //swap the last element with element at random index
+                temp = r[i];
+                r[i] = r[j];
+                r[j] = temp;
+        }
+   }
